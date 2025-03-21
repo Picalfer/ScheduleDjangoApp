26b8b0c1798540e33addc32adfa77ab95a846d10
Arthur Kozhemyakin <trable228@icloud.com>
2025-03-21 12:39:39 +0300

    login, register, change password, refactor

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,15 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.hello, name='home'),
     path('get-data/', views.get_data, name='get_data'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,54 @@
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-def index(request):
-    return render(request, 'index.html')
+from .forms import RegisterForm, LoginForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Автоматически входим после регистрации
+            return redirect('home')  # Перенаправляем на главную страницу
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Перенаправляем на главную страницу
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')  # Перенаправляем на главную страницу
+
+
+def hello(request):
+    return render(request, 'core/hello.html')
+
 
 def get_data(request):
     response_data = {
         'message': 'Данные с бэкенда!',
     }
     return JsonResponse(response_data)
+
+
+@login_required
+def profile(request):
+    return render(request, 'core/profile.html', {'user': request.user})
