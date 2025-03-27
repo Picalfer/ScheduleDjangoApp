3ca62d62d368d75b4006389e0650febff9e2ad4c
Arthur Kozhemyakin <trable228@icloud.com>
2025-03-27 21:41:41 +0300

    use russian in admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,122 +1,28 @@
-from io import BytesIO
-
+from django import forms
 from django.contrib import admin
-from django.http import HttpResponse
-from django.utils.timezone import now
-from openpyxl import Workbook
 
-from core.models import Teacher, Student, TimeSlot, LessonLog
-
-
-@admin.register(LessonLog)
-class LessonLogAdmin(admin.ModelAdmin):
-    list_display = ('id', 'lesson', 'student', 'teacher', 'action', 'created_at')
-    list_filter = (
-        'action',
-        ('teacher', admin.RelatedOnlyFieldListFilter),
-        ('student', admin.RelatedOnlyFieldListFilter),
-        'created_at'
-    )
-    search_fields = ('student__name', 'teacher__username', 'notes')
-    readonly_fields = ('created_at', 'old_balance', 'new_balance')
-    date_hierarchy = 'created_at'
-    actions = ['export_logs']  # Регистрируем действие
-
-    # Метод для экспорта логов
-
-    def export_logs(self, request, queryset):
-        wb = Workbook()
-        ws = wb.active
-        ws.title = "Логи уроков"
-
-        # Заголовки
-        ws.append([
-            'ID', 'ID урока', 'Студент',
-            'Преподаватель', 'Действие',
-            'Было уроков', 'Стало уроков',
-            'Дата проведения', 'Примечания'
-        ])
-
-        # Данные
-        for log in queryset:
-            ws.append([
-                log.id,
-                log.lesson.id,
-                log.student.name,
-                log.teacher.get_full_name() or log.teacher.username,
-                log.get_action_display(),
-                log.old_balance,
-                log.new_balance,
-                log.created_at.strftime('%d.%m.%Y %H:%M'),
-                log.notes
-            ])
-
-        # Настройка ширины колонок
-        for column in ws.columns:
-            max_length = 0
-            for cell in column:
-                try:
-                    if len(str(cell.value)) > max_length:
-                        max_length = len(str(cell.value))
-                except:
-                    pass
-            ws.column_dimensions[column[0].column_letter].width = max_length + 2
-
-        buffer = BytesIO()
-        wb.save(buffer)
-        buffer.seek(0)
-
-        response = HttpResponse(
-            buffer.getvalue(),
-            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        )
-        response['Content-Disposition'] = 'attachment; filename=lesson_logs_%s.xlsx' % now().strftime('%Y-%m-%d')
-        return response
-
-    export_logs.short_description = "Экспорт выбранных логов в CSV"
-
-    fieldsets = (
-        (None, {
-            'fields': ('lesson', 'teacher', 'student', 'action')
-        }),
-        ('Баланс', {
-            'fields': ('old_balance', 'new_balance')
-        }),
-        ('Дополнительно', {
-            'fields': ('created_at', 'notes')
-        }),
-    )
+from core.models import Teacher, Student, Lesson
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    list_display = ('user', 'courses')
-    search_fields = ('user__username', 'courses')
+    list_display = ('id', 'get_full_name', 'user')
+
+    def get_full_name(self, obj):
+        return obj.user.get_full_name()  # Метод для отображения ФИО
+
+    get_full_name.short_description = 'ФИО'  # Заголовок колонки
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'subject', 'teacher', 'bitrix_link')
-    list_filter = ('subject', 'teacher')
-    search_fields = ('name', 'subject')
+    list_display = ('name', 'teacher', 'bitrix_link', 'lesson_balance')
 
 
-class LessonLogInline(admin.TabularInline):
-    model = LessonLog
-    extra = 0
-    readonly_fields = ('teacher', 'student', 'action', 'created_at')
-    can_delete = False
-
-
-from django import forms
-from django.contrib import admin
-
-
-@admin.register(TimeSlot)
-class TimeSlotAdmin(admin.ModelAdmin):
-    list_display = ('date', 'start_date', 'time', 'teacher', 'student', 'subject', 'is_recurring_display')
-    list_filter = ('is_recurring', 'status', 'date')
-    inlines = [LessonLogInline]
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('date', 'lesson_type', 'teacher', 'start_date', 'time', 'student')
+    list_filter = ('lesson_type', 'status', 'date')
 
     def save_model(self, request, obj, form, change):
         if obj.is_recurring and not obj.start_date:
