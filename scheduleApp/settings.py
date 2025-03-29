6d1ccd5f95aa9d687a10fe881a02f62dc401ab19
Arthur Kozhemyakin <trable228@icloud.com>
2025-03-29 17:56:32 +0300

    add logging

--- scheduleApp/settings.py
+++ scheduleApp/settings.py
@@ -85,6 +85,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'scheduleApp.wsgi.application'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
