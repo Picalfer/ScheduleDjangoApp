b5afc197fb6e77f8882b8f98d0e7bb06314c1a0a
Arthur Kozhemyakin <trable228@icloud.com>
2025-04-09 16:01:17 +0300

    fix settings

--- scheduleApp/settings.py
+++ scheduleApp/settings.py
@@ -176,7 +176,7 @@ FORMAT_MODULE_PATH = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
