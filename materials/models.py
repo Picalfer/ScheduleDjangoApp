67aef20dad6848bf38391b4f9edbcee4dcc16da1
Arthur Kozhemyakin <trable228@icloud.com>
2025-04-17 14:00:02 +0300

    cover - svg

--- materials/models.py
+++ materials/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField("Название курса", max_length=100)
     short_description = models.TextField("Краткое описание", max_length=200)
-    cover = models.ImageField("Обложка", upload_to="courses/covers/", blank=True)
+    cover = models.FileField(
+        "Обложка",
+        upload_to="courses/covers/",
+        blank=True,
+        help_text="Загрузите SVG файл для обложки"
+    )
     slug = models.SlugField("URL-адрес", unique=True)  # Для красивых URL
     order = models.PositiveIntegerField("Порядок", default=0)  # Для сортировки
 
