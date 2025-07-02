f825ea9ff21c2977d7732fa88a2434eb0278daf5
Arthur Kozhemyakin <trable228@icloud.com>
2025-07-02 12:04:50 +0300

    add rest_framework to INSTALLED_APPS

--- scheduleApp/settings.py
+++ scheduleApp/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'rest_framework',
     'django_admin_inline_paginator',
     'django.contrib.sessions',
     'django.contrib.messages',
