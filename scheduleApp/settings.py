633605f858e70980eb6a2dc066dd7ace4788906c
Arthur Kozhemyakin <trable228@icloud.com>
2025-09-10 15:52:35 +0300

    test new icon

--- scheduleApp/settings.py
+++ scheduleApp/settings.py
@@ -241,21 +241,21 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
-        'src': '/static/core/images/icons/152.png',
+        'src': '/static/core/images/icons/ic_launcher_160x160.png',
         'sizes': '152x152',
         'type': 'image/png',
     }
 ]
 PWA_APP_ICONS_APPLE = [
     {
-        'src': '/static/core/images/icons/152.png',
+        'src': '/static/core/images/icons/ic_launcher_160x160.png',
         'sizes': '152x152',
         'type': 'image/png',
     }
 ]
 PWA_APP_SPLASH_SCREEN = [
     {
-        'src': '/static/core/images/icons/152.png',
+        'src': '/static/core/images/icons/ic_launcher_160x160.png',
         'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
     }
 ]
